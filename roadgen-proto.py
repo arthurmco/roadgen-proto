4b3529afd74f0ff8567883f29ef503cb54ce18af
Arthur Mendes <arthurmco@gmail.com>
2020-07-31 22:14:56 -0300

    Do not generate paths or "centers" on water

--- roadgen-proto.py
+++ roadgen-proto.py
@@ -4,10 +4,13 @@ import random
 
 
 terrain_types = {
-    "water": (96, 128, 255),
+    "deep_water": (6, 38, 165),
     "grass": (32, 164, 32),
 }
 
+
+def is_terrain_buildable(old_value):
+    return old_value != terrain_types['deep_water']
 
 def get_center_offset_from_seed(seed):
     """
@@ -20,12 +23,16 @@ def get_center_offset_from_seed(seed):
 
     return dx, dy
 
-def get_distance_from_center(x, y, seed):
+def get_distance_from_center(x, y, old_value, seed):
     """
     Check the distance from the closest "center" for a given coordinate
 
     A center occurs on 200-pixel intervals.
     """
+    if not is_terrain_buildable(old_value):
+        # TODO: if possible, return the distance to the closest non-water center?
+        return 200, 200 
+
     dx, dy = get_center_offset_from_seed(seed)
     
     closestx = (round(x / 200) * 200) + dx
@@ -33,12 +40,15 @@ def get_distance_from_center(x, y, seed):
 
     return closestx-x, closesty-y
 
-def is_path(x, y, seed):
+def is_path(x, y, old_value, seed):
     """
     Check if the position is on a "path"
     """
     dx, dy = get_center_offset_from_seed(seed)
-    
+   
+    if not is_terrain_buildable(old_value):
+        return False    
+
     if (x % 200 == dx or y % 200 == dy) and x > 0 and y > 0:
         return True
 
@@ -51,14 +61,14 @@ def generate(x, y, old_value, seed):
 
     Return the new color.
     """
-    dx, dy = get_distance_from_center(x, y, seed)
+    dx, dy = get_distance_from_center(x, y, old_value, seed)
     if abs(dx) < 30 and abs(dy) < 30:
         if abs(dx) < 15 and abs(dy) < 15:
             return (250, 125, 0)
 
         return (230, 108, 0)
 
-    if is_path(x, y, seed):
+    if is_path(x, y, old_value, seed):
         return (255, 255, 255)
 
     return old_value
@@ -82,7 +92,7 @@ def generate_terrain(img, seed):
     It is not the core of the script.
     """
     i = 0
-    rndarr = range(0, abs(seed)+1, round(seed/65536))
+    rndarr = range(0, abs(seed)+1, round(abs(seed)/65536))
 
     def get_next():
         nonlocal i
@@ -91,22 +101,22 @@ def generate_terrain(img, seed):
         return rndarr[i]
 
 
-    def is_water(x, y, a, b, c, is_negative):
-        waterinterval = 128 + (c % 1024)
-        waterx = a % 512
-        watery = b % 512
-        watersize = a % 128 + b % 128
+    def is_deep_water(x, y, a, b, c, is_negative):
+        deep_waterinterval = 128 + (c % 1024)
+        deep_waterx = a % 512
+        deep_watery = b % 512
+        deep_watersize = a % 128 + b % 128
 
-        print(f"\rwi: {waterinterval}, wx: {waterx}, wy: {watery}, wsize: {watersize}", end="")
+        print(f"\rwi: {deep_waterinterval}, wx: {deep_waterx}, wy: {deep_watery}, wsize: {deep_watersize}", end="")
 
-        rx = x % waterinterval
-        ry = y % waterinterval
+        rx = x % deep_waterinterval
+        ry = y % deep_waterinterval
 
         if is_negative:
-            watersize = watersize * 2
+            deep_watersize = deep_watersize * 2
 
-        if rx > waterx-watersize and rx < waterx+watersize and \
-            ry > watery-watersize and ry < watery+watersize:
+        if rx > deep_waterx-deep_watersize and rx < deep_waterx+deep_watersize and \
+            ry > deep_watery-deep_watersize and ry < deep_watery+deep_watersize:
                 return True
 
         return False
@@ -118,7 +128,7 @@ def generate_terrain(img, seed):
     w, h = img.size
     for y in range(h):
         for x in range(w):
-            pixel = terrain_types['water'] if is_water(x, y, a, b, c,
+            pixel = terrain_types['deep_water'] if is_deep_water(x, y, a, b, c,
                 seed < 0) is True else terrain_types['grass']
             img.putpixel((x, y), generate(x, y, pixel, seed))
 
@@ -136,7 +146,7 @@ if len(sys.argv) > 1:
     seed = int(sys.argv[1])
 else:
     seed = create_seed()
-    
+
 print(f"your seed is {seed}")
 
 terrain_image = Image.new("RGB", (1600, 1200), (0, 0, 0))
