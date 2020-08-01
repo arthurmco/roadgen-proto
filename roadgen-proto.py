eb517451102bc529806d31396eb42bacae123ab1
Arthur Mendes <arthurmco@gmail.com>
2020-07-31 21:02:35 -0300

    Generate small squares near the center of each "center"

--- roadgen-proto.py
+++ roadgen-proto.py
@@ -2,14 +2,29 @@ import sys
 from PIL import Image
 
 
+def get_distance_from_center(x, y):
+    """
+    Check the distance from the closest "center" for a given coordinate
+
+    A center occurs on 200-pixel intervals.
+    """
+    closestx = round(x / 200) * 200
+    closesty = round(y / 200) * 200
+
+    return closestx-x, closesty-y
+
 def generate(x, y, old_value):
     """
     Generate the pixel color from an position and the old value.
 
     Return the new color.
     """
-    if x % 100 == 0:
-        return (255, 0, 0)
+    dx, dy = get_distance_from_center(x, y)
+    if abs(dx) < 30 and abs(dy) < 30:
+        if abs(dx) < 15 and abs(dy) < 15:
+            return (250, 125, 0)
+
+        return (230, 108, 0)
 
     return old_value
 
